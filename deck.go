439e8d038d16598d2fed89c078ceb3f595ef0c7e
Bogdan Filimon <bogdanfilimon@Bogdans-MacBook-Pro.local>
2023-06-25 17:20:33 +0300

    new deck from file

--- deck.go
+++ deck.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 type deck []string
 
@@ -25,4 +29,21 @@ func (d deck) print() {
 
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return os.WriteFile(filename, []byte(d.toString()), 0660)
+}
+
+func newDeckFromFile(filename string) deck {
+	cardBytes, error := os.ReadFile(filename)
+	if error != nil {
+		fmt.Println("Error:", error)
+		os.Exit(1)
+	}
+	return deck(strings.Split(string(cardBytes), ","))
 }
--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 func main() {
-	cards := newDeck()
+	// cards := newDeck()
 
-	hand, remainingDeck := deal(cards, 5)
-	hand.print()
-	remainingDeck.print()
+	// hand, remainingDeck := deal(cards, 5)
+	// hand.print()
+	// remainingDeck.print()
+	// greeting := "Hi there!"
+	// fmt.Println([]byte(greeting))
+	// fmt.Println(cards.toString())
+	// cards.saveToFile("cards.txt")
+	newDeckFromFile("cards.txt").print()
 }
